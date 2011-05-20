b2537b294de50f22ecd606d5e7939b5e05bb6298
Reem Helou <rhel91@gmail.com>
2011-05-20 08:46:59 -0400

    Made a rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FamilyQuilt::Application.config.secret_token = 'd9ab5251b10cade8ba28e8d000642c3fcc137a782b7f6638e4494f7cd142547aea591792d1435cf10cb77bffe4afc716769959d7e6f78c43118b24b49393f708'
